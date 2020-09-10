36a3e2285398e5249a10f95d5aecfd1ec153bb70
David Tounn <7319dtounn@gmail.com>
2020-09-10 16:29:16 -0700

    new

---
+++ IntroAssessment/IntroAssessment/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace IntroAssessment
+{
+    class Player
+    {
+        public int gold = 1000;
+        public string inv;
+        
+        
+    }
+}
--- IntroAssessment/IntroAssessment/Program.cs
+++ IntroAssessment/IntroAssessment/Program.cs
@@ -5,10 +5,16 @@ using System.Net.NetworkInformation;
 using CsvHelper;
 using System.Globalization;
 using System.Linq;
-
+using System.Collections.Generic;
 
 namespace IntroAssessment
 {
+    struct PlayerInv
+    {
+        string name;
+        public string Name { get => name; set => name = value; }
+    }  
+       
     struct ShopWeaponsInv
     {
         string name;
@@ -28,6 +34,7 @@ namespace IntroAssessment
         static void Main(string[] args)
         {
             ShopWeaponsInv[] WeaponInShop;
+            PlayerInv[] inv;
             using (var reader = new StreamReader("Weapons.csv"))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
@@ -35,23 +42,39 @@ namespace IntroAssessment
                  csv.GetRecords<ShopWeaponsInv>();
                 WeaponInShop = records.ToArray<ShopWeaponsInv>();
             }
+            using (var reader = new StreamReader("playerinv.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                System.Collections.Generic.IEnumerable<PlayerInv> records =
+                 csv.GetRecords<PlayerInv>();
+                inv = records.ToArray<PlayerInv>();
+            }
+            
+            Player player = new Player();
 
 
-                Console.WriteLine("Weclome to my shop, what would you like to do?");
-            bool gameover = false;
+            Console.WriteLine("Weclome to my shop, what would you like to do?");
+            bool menu = true;
+            bool shopping = false;
             // Game Loop
-            while(!gameover)
+            while(menu)
             {
                
                 string command = Console.ReadLine();
                 // Player Input 
                 if (command == "buy")
                 {
+                    
+                    shopping = true;
                     Console.WriteLine("what would you like to buy?");
-                    foreach(ShopWeaponsInv tmp in WeaponInShop)
+                    foreach (ShopWeaponsInv tmp in WeaponInShop)
                     {
-                        Console.WriteLine(tmp.Name);
+                        Console.WriteLine(tmp.Name + ": c" + tmp.Price + " gold");
+                        
+
                     }
+                    menu = false;
+
 
                 }
                 else if (command == "sell")
@@ -62,14 +85,37 @@ namespace IntroAssessment
                 else if (command == "leave")
                 {
                     Console.WriteLine("do come again");
-                    gameover = true;
+                    menu = false;
 
 
+                }
+                else if (command == "inv")
+                {
+                    foreach (PlayerInv tmp in inv)
+                    {
+                        Console.WriteLine(tmp.Name);
+                    }
+                    Console.WriteLine("gold: " + player.gold);
                 }
                 // if player type anything that not in the game
                 else
                 {
                     Console.WriteLine("invadle");
+                }
+                while(shopping)
+                {
+                    string command2 = Console.ReadLine();
+                    if(command2 == "ironsword")
+                    {
+                        
+                        Console.WriteLine("You bought an iron sword, you are now broke");
+                        
+                    }
+                    else if (command2 == "leave")
+                    {
+                        shopping = false;
+                        menu = true;
+                    }
                 }
 
               }
